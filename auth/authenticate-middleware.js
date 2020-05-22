3c14622b81009cec730ee702ec4ef4753ccbe35e
Diddleslip <diddleslip@gmail.com>
2020-05-22 13:56:20 -0400

    Finished MVP

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,28 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require("jsonwebtoken");
+
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  // Add code here to verify users are logged in
+  const token = req.headers.authorization;
+
+  if(token) {
+    const secret = process.env.JWT_SECRET || "shhh";
+
+    jwt.verify(token, secret, (error, decodedToken) => {
+      if(error) {
+        // The token is invalid
+        res.status(401).json({ you: 'shall not pass! 401' });
+      } else {
+        // The token is good
+        req.jwt = decodedToken;
+        
+        next();
+      }
+    })
+  } else {
+    res.status(400).json({ message: "You cannot pass! 401" });
+  }
 };
